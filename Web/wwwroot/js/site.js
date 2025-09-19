9867e5ec22462f703827c690add1bd9a5ef9b03d
Nicolas Silva <nicolas.silva@bkbank.com.br>
2025-09-19 16:35:16 -0300

    feat: controller de menu

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('.document-mask').mask('000.000.000-00');     // CPF
+    $('.cellPhone-mask').mask('(00) 00000-0000');   // Celular
+    $('.zipCode-mask').mask('00000-000');           // CEP
+});
